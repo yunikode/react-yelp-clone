2a1990747a45c20e07c5100a40f3cf360c1eee6a
zee-german <franz.lutzenberger@gmail.com>
2016-05-30 18:48:39 +0200

    Routing in place

---
+++ src/containers/App/App.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react'
+import { Router } from 'react-router'
+
+class App extends React.Component {
+  static contextTypes = {
+    router:PropTypes.object
+  }
+
+  static propTypes = {
+    routes: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
+
+  // class getter
+  get content() {
+    return (
+      <Router
+        routes={this.props.routes}
+        history={this.props.history}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100%' }}>
+        {this.content}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ tests.webpack.js
@@ -0,0 +1,12 @@
+require('babel-polyfill')
+
+// some setup first
+
+var chai = require('chai')
+var chaiEnzyme = require('chai-enzyme')
+
+chai.use(chaiEnzyme())
+
+var context = require.context('./src', true, /\.spec\.js$/)
+context.keys().forEach(context)
+module.exports = context
